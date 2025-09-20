dbad720d6b136ace936bfa87816702a563857c82
aqua-4 <parasharsangle@gmail.com>
2025-09-21 00:00:10 +0530

    ENH: deployed base app
    Changes to be committed:
    	modified:   index.html
    	modified:   package.json
    	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/telegram_algo_trader_llm/',   // 👈 IMPORTANT
 })
